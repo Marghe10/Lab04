68d60bcb959ae8ace1a29ca7848f83462cbbaa1d
Marghe10 <s340927@studenti.polito.it>
2025-10-19 23:48:46 +0200

    creata classe passeggeri

---
+++ passeggeri.py
@@ -0,0 +1,6 @@
+class Passeggeri:
+    def __init__(self,codicePasseggero, nome, cognome):
+        self.codice = codicePasseggero
+        self.nome = nome
+        self.cognome = cognome
+        self.assegnato = None
